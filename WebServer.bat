4c3a940da4e4a375076938afe8e70b3c153c4b90
3P <78256817+3PSY0N@users.noreply.github.com>
2023-08-18 01:31:42 +0200

    Add: main batch program

---
+++ WebServer.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+:: Generate Random port
+:generate_port
+set /a "port=%RANDOM% * (65535 - 1024 + 1) / 32767 + 1024"
+
+:: Check if port is already used
+netstat -an | find "%port%" > nul
+if not errorlevel 1 (
+    goto generate_port
+)
+
+:: Get current directory (where context menu was clicked)
+set "current_directory=%cd%"
+
+:: Start PHP Server
+start "Serveur PHP" cmd /c "php -S localhost:%port% -t "%current_directory%""
+
+:: Start Browser and open server URL
+start "Browser" "http://localhost:%port%"
+
+endlocal
